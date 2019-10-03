2b9fcb11343a500db4777dd49eee10bde4cca388
CharZakaria <Z.charouh@gmail.com>
2019-10-03 16:52:40 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 class Configuration:
 	def _init_(self):
 		self.clusters_centroid = [(140,164),(224,234),(414,362),(425,531)]
-		self.input_shapes= [(200,280),(370,380),(480,450),(480,748)]
+		self.input_shapes= [(224,288),(352,384),(480,480),(480,768)]
